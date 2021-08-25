6363a2f94462547425cd6a57a70b3f42ef0c33b3
rmit <rmit@qmac.local>
2021-08-25 16:28:39 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+ //https://www.digitalocean.com/community/tutorials/how-to-add-login-authentication-to-react-applications
+  //đổi tên folder và file khác nhau so với bài hướng dẫn, dấu chấm trc componets chỉ có 1 (không phải 2 như trong bài)
+import React from 'react';
 import './App.css';
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
+import Preferences from './components/Preferences';
 
-function App() {
+import userToken from './userToken';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+
+
+function App(){
+  
+
+  const { token, setToken } = userToken();
+ 
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    
+      <div className="wrapper">
+        <h1>Application</h1>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/dashboard">
+              <Dashboard />
+            </Route>
+            <Route path="/preferences">
+              <Preferences />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </div>
+    
   );
 }
 
